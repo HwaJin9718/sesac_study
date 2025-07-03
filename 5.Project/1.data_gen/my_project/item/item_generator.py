f836dd7260eacc3cea8b53f6cf8edfbaa06f4eaa
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-03 22:13:07 +0900

    250703_Project_미니_CRM데이터 생성기_리펙토링(txt 파일 정리, 불필요 주석 삭제)

--- 5.Project/1.data_gen/my_project/item/item_generator.py
+++ 5.Project/1.data_gen/my_project/item/item_generator.py
@@ -5,8 +5,8 @@ from item.price_generator import PriceGenerator
 class ItemGenerator:
     def __init__(self):
         self.id_gen = IdGenerator()
-        self.name_gen = NameGenerator('itemname.txt', 'itemtype.txt')
-        self.price_gen = PriceGenerator('price.txt')
+        self.name_gen = NameGenerator('db/itemname.txt', 'db/itemtype.txt')
+        self.price_gen = PriceGenerator('db/price.txt')
         
     def generate_item(self, count):
         items = []
@@ -17,15 +17,3 @@ class ItemGenerator:
             price = self.price_gen.generate_price()
             items.append((id, itemname, itemtype, price))
         return items
-
-
-# Id,                                  Name,           Type,  UnitPrice
-# aeaa187d-f30d-42db-8c23-7632f85bacef,Americano Coffee,Coffee,3000
-# f543b3a2-9409-4ba5-be23-75834a916962,Strawberry Cake,Cake,5500
-# d245b380-6572-4404-b809-55eea2daf7d0,Watermelon Juice,Juice,4000
-# 022f1eec-5257-41dc-9985-0e289ff21046,Americano Coffee,Coffee,3000
-# f1b56583-8a27-4a07-89d1-ded18794bbb6,Strawberry Cake,Cake,5500
-# 8fa762e7-4898-43dd-a72c-253a043cc84c,Americano Coffee,Coffee,3000
-# a7f9b9d0-cb48-43f0-9186-b6d77211a7c0,Espresso Coffee,Coffee,2500
-# e530a804-38d6-4981-9bfe-60f3b4d26da9,Vanilla Cake,Cake,5000
-# b285ccfc-8d3e-48aa-a69f-34ecae62ccb4,Red Velvet Cake,Cake,6500
--- 5.Project/1.data_gen/my_project/store/store_generator.py
+++ 5.Project/1.data_gen/my_project/store/store_generator.py
@@ -5,8 +5,8 @@ from store.address_generator import AddressGenerator
 class StoreGenerator:
     def __init__(self):
         self.id_gen = IdGenerator()
-        self.name_gen = NameGenerator('store.txt', 'cities.txt')
-        self.address_gen = AddressGenerator('cities.txt')
+        self.name_gen = NameGenerator('db/store.txt', 'db/cities.txt')
+        self.address_gen = AddressGenerator('db/cities.txt')
         
     def generate_store(self, count):
         stores = []
@@ -17,14 +17,3 @@ class StoreGenerator:
             address = self.address_gen.generate_address()
             stores.append((id, name, cafename, address))
         return stores
-
-# Id,Name,Type,Address
-# 69568455-8856-4a61-b7a0-8e2061fd06cd,스타벅스 홍대8호점,스타벅스,부산 강남구 31로 50
-# a7ba6be3-356b-422c-9250-d2ba95c470da,스타벅스 송파2호점,스타벅스,인천 강서구 15길 42
-# 6d90b46e-7bab-45e3-ab88-9ff1eb393aee,투썸 강서9호점,투썸,부산 강남구 25로 57
-# e687ed96-5716-451d-ba80-75e5d107c2ad,커피빈 신촌5호점,커피빈,부산 강서구 32로 45
-# 72a8418b-0f81-4ee7-b57e-b8d9ff7eab73,이디야 잠실9호점,이디야,부산 강남구 74길 79
-# 7951a2e8-5384-4132-8d70-0d027f976597,스타벅스 송파9호점,스타벅스,부산 강남구 43로 71
-# 3a7566d7-ff3b-4698-b850-727c1374a7ea,커피빈 잠실2호점,커피빈,광주 강남구 74길 74
-# 26be172c-ae2e-48a6-8613-951761be4494,이디야 잠실4호점,이디야,부산 강남구 100로 56
-# 0acc9d84-726b-4e53-a27f-883759456f79,커피빈 홍대3호점,커피빈,광주 중구 91길 95
