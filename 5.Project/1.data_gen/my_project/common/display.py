36141967adf8a1fa6aac1b2d4d05b0374da846b2
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-03 23:32:56 +0900

    250703_Project_미니_CRM데이터 생성기_리펙토링(main.py 내 DisplayData 분리)

---
+++ 5.Project/1.data_gen/my_project/common/display.py
@@ -0,0 +1,82 @@
+import csv
+
+from user.user_generator import UserGenerator
+from store.store_generator import StoreGenerator
+from item.item_generator import ItemGenerator
+from order.order_generator import OrderGenerator
+from orderitem.orderitem_generator import OrderItemGenerator
+
+class UserDisplayData(UserGenerator):
+    def print_console(self, count):
+        data = self.generate_user(count)
+        for id, name, birthdate, age, gender, address in data:
+            print(f"ID: {id}\nName: {name}\nBirthdate: {birthdate}\nAge: {age}\nGender: {gender}\nAddress: {address}\n")
+
+    def print_csv(self, count, file_path, top_data):
+        data = self.generate_user(count)
+
+        with open(file_path, "w", newline = "", encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(top_data)
+            csv_writer.writerows(data)
+        print(f"CSV 파일 저장 완료")
+
+class StoreDisplayData(StoreGenerator): 
+    def print_console(self, count):
+        data = self.generate_store(count)
+        for id, name, cafename, address in data:
+            print(f"ID: {id}\nName: {name}\nCafeName: {cafename}\nAddress: {address}\n")
+
+    def print_csv(self, count, file_path, top_data):
+        data = self.generate_store(count)
+
+        with open(file_path, "w", newline = "", encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(top_data)
+            csv_writer.writerows(data)
+        print(f"CSV 파일 저장 완료")
+
+class ItemDisplayData(ItemGenerator): 
+    def print_console(self, count):
+        data = self.generate_item(count)
+        for id, itemname, itemtype, price in data:
+            print(f"ID: {id}\nItemName: {itemname}\nItemType: {itemtype}\nPrice: {price}\n")
+
+    def print_csv(self, count, file_path, top_data):
+        data = self.generate_item(count)
+
+        with open(file_path, "w", newline = "", encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(top_data)
+            csv_writer.writerows(data)
+        print(f"CSV 파일 저장 완료")
+
+class OrderDisplayData(OrderGenerator):
+    def print_console(self, count):
+        data = self.generate_order(count)
+        for id, order_at, store_id, user_id in data:
+            print(f"ID: {id}\nOrderAt: {order_at}\nStoreId: {store_id}\nUserId: {user_id}\n")
+
+    def print_csv(self, count, file_path, top_data):
+        data = self.generate_order(count)
+
+        with open(file_path, "w", newline = "", encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(top_data)
+            csv_writer.writerows(data)
+        print(f"CSV 파일 저장 완료")
+
+class OrderItemDisplayData(OrderItemGenerator):
+    def print_console(self, count):
+        data = self.generate_orderitem(count)
+        for id, order_id, item_id in data:
+            print(f"ID: {id}\nOrderId: {order_id}\nItemId: {item_id}\n")
+
+    def print_csv(self, count, file_path, top_data):
+        data = self.generate_orderitem(count)
+
+        with open(file_path, "w", newline = "", encoding="utf-8") as file:
+            csv_writer = csv.writer(file)
+            csv_writer.writerow(top_data)
+            csv_writer.writerows(data)
+        print(f"CSV 파일 저장 완료")
--- 5.Project/1.data_gen/my_project/main.py
+++ 5.Project/1.data_gen/my_project/main.py
@@ -2,95 +2,16 @@
 
 import sys
 import csv
-from user.user_generator import UserGenerator
-from store.store_generator import StoreGenerator
-from item.item_generator import ItemGenerator
-from order.order_generator import OrderGenerator
-from orderitem.orderitem_generator import OrderItemGenerator
+from common.display import UserDisplayData, StoreDisplayData, ItemDisplayData, OrderDisplayData, OrderItemDisplayData
 
 # 사용자 입력 값
 data_type = input(("데이터 유형을 입력하세요 (User, Store 또는 Item): "))
-num = int(input("생성할 데이터 개수를 입력하세요: "))
+count = int(input("생성할 데이터 개수를 입력하세요: "))
 output_type = input(("아웃풋 형태를 입력하세요 (csv, console): "))
 
 my_data = None
 file_path = ""
 top_data = []
-
-class UserDisplayData(UserGenerator):
-    def print_console(self, count):
-        data = self.generate_user(count)
-        for id, name, birthdate, age, gender, address in data:
-            print(f"ID: {id}\nName: {name}\nBirthdate: {birthdate}\nAge: {age}\nGender: {gender}\nAddress: {address}\n")
-
-    def print_csv(self, count):
-        data = self.generate_user(count)
-
-        with open(file_path, "w", newline = "", encoding="utf-8") as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(top_data)
-            csv_writer.writerows(data)
-        print(f"CSV 파일 저장 완료")
-
-class StoreDisplayData(StoreGenerator): 
-    def print_console(self, count):
-        data = self.generate_store(count)
-        for id, name, cafename, address in data:
-            print(f"ID: {id}\nName: {name}\nCafeName: {cafename}\nAddress: {address}\n")
-
-    def print_csv(self, count):
-        data = self.generate_store(count)
-
-        with open(file_path, "w", newline = "", encoding="utf-8") as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(top_data)
-            csv_writer.writerows(data)
-        print(f"CSV 파일 저장 완료")
-
-class ItemDisplayData(ItemGenerator): 
-    def print_console(self, count):
-        data = self.generate_item(count)
-        for id, itemname, itemtype, price in data:
-            print(f"ID: {id}\nItemName: {itemname}\nItemType: {itemtype}\nPrice: {price}\n")
-
-    def print_csv(self, count):
-        data = self.generate_item(count)
-
-        with open(file_path, "w", newline = "", encoding="utf-8") as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(top_data)
-            csv_writer.writerows(data)
-        print(f"CSV 파일 저장 완료")
-
-class OrderDisplayData(OrderGenerator):
-    def print_console(self, count):
-        data = self.generate_order(count)
-        for id, order_at, store_id, user_id in data:
-            print(f"ID: {id}\nOrderAt: {order_at}\nStoreId: {store_id}\nUserId: {user_id}\n")
-
-    def print_csv(self, count):
-        data = self.generate_order(count)
-
-        with open(file_path, "w", newline = "", encoding="utf-8") as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(top_data)
-            csv_writer.writerows(data)
-        print(f"CSV 파일 저장 완료")
-
-class OrderItemDisplayData(OrderItemGenerator):
-    def print_console(self, count):
-        data = self.generate_orderitem(count)
-        for id, order_id, item_id in data:
-            print(f"ID: {id}\nOrderId: {order_id}\nItemId: {item_id}\n")
-
-    def print_csv(self, count):
-        data = self.generate_orderitem(count)
-
-        with open(file_path, "w", newline = "", encoding="utf-8") as file:
-            csv_writer = csv.writer(file)
-            csv_writer.writerow(top_data)
-            csv_writer.writerows(data)
-        print(f"CSV 파일 저장 완료")
 
 # 데이터 유형 확인
 if data_type.lower() == "user":
@@ -118,9 +39,9 @@ else:
 
 # 아웃풋 형태 확인
 if output_type.lower() == "csv":
-    my_data.print_csv(num)
+    my_data.print_csv(count, file_path, top_data)
 elif output_type.lower() == "console":
-    my_data.print_console(num)
+    my_data.print_console(count)
 else:
     print("지원되지 않는 출력 형태입니다.")
 
