a6cfcb7932849526d5306e28b946977b6fd94a22
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-10 01:57:30 +0900

    250709_Project_Flask_숙제 pixabay jinja2 template 어드민 페이지 화면 및 기능 구현 완료

--- 5.Project/2.pixabay/4.frontend_admin_template/app.py
+++ 5.Project/2.pixabay/4.frontend_admin_template/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify, url_for, render_template, request
+from flask import Flask, jsonify, url_for, render_template, request, redirect
+import os
 
 app = Flask(__name__)
 
@@ -12,6 +13,14 @@ images = [
     {"filename" : "fox.jpg", "keywords" : ["fox", "animal", "eye", "big ear"]},
     {"filename" : "panda.jpg", "keywords" : ["panda", "animal", "eat", "hand"]},
 ]
+
+app.config['UPLOAD_FOLDER'] = 'static/img'
+app.config['ALLOWED_FILE_EXT'] = {'png', 'jpg', 'jpeg', 'gif', 'png'}
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+def allowed_file_pythonic(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_FILE_EXT']
 
 @app.route('/')
 def index():
@@ -46,15 +55,80 @@ def admin():
 
 @app.route('/upload', methods=["POST"])
 def upload():
-    return 'upload 호출'
 
-@app.route('/modify')
+    file = request.files['file']
+    keywords = request.form.get('keywords')
+    # print(keywords)
+
+    if file.filename == '':
+        return '파일이 올바르게 전송되지 않았습니다.'
+    
+    # 1. 사진 파일만 업로드 가능하게 한다.
+    if allowed_file_pythonic(file.filename):
+        filepath = os.path.join('./', app.config['UPLOAD_FOLDER'], file.filename)
+        file.save(filepath)
+
+        # 리스트에 해당 값 저장
+        if keywords:
+            keywords_list = [keyword.strip() for keyword in keywords.split(',')]
+        else:
+            keywords_list = []
+
+        images.append({ "filename" : file.filename  ,  "keywords" : keywords_list })
+
+        return redirect(url_for('admin'))
+    else:
+        return '하용되지 않는 파일입니다.'
+
+
+@app.route('/modify', methods=["POST"])
 def keywords_modify():
-    return 'modify 호출'
 
-@app.route('/remove')
-def remove():
-    return 'remove 호출'
+    filename = request.form.get('filename')
+    new_keywords = request.form.get('keywords')
+
+    if new_keywords:
+        keywords_list = [keyword.strip() for keyword in new_keywords.split(',')]
+    else:
+        keywords_list = []
+
+    for img in images:
+        if img["filename"] == filename:
+            img["keywords"] = keywords_list
+            break
+
+    return redirect(url_for('admin'))
+
+
+@app.route('/remove/<filename>')
+def remove(filename):
+
+    filepath = os.path.join('./', 'static/img', filename)
+
+    if os.path.exists(filepath):
+        # 실제 이미지 삭제
+        os.remove(filepath)
+        
+        # 리스트에서 filename 삭제
+        for img in images[:]:
+            if img["filename"] == filename:
+                images.remove(img)
+
+        return redirect(url_for('admin'))
+    else:
+        return '해당 파일은 존재하지 않습니다.'
+
+# @app.route('/upload', methods=["POST"])
+# def upload():
+#     return 'upload 호출'
+
+# @app.route('/modify')
+# def keywords_modify():
+#     return 'modify 호출'
+
+# @app.route('/remove')
+# def remove():
+#     return 'remove 호출'
 
 if __name__ == '__main__':
     app.run(debug=True)
