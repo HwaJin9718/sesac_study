b9740eda6529af16417a2195b429346a7faba8a4
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-09 20:24:24 +0900

    250709_Project_Flask_간단한 pixabay 클론코딩 / pixabay 숙제 기본 코드

---
+++ 5.Project/2.pixabay/2.frontend_template/app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, url_for, render_template, request
+
+app = Flask(__name__)
+
+images = [
+    {"filename" : "cat.jpg", "keywords" : ["cat", "animal", "pet", "cute", "baby"]},
+    {"filename" : "cat2.jpg", "keywords" : ["cat", "animal", "pet", "two", "baby", "sleep"]},
+    {"filename" : "cat3.jpg", "keywords" : ["cat", "animal", "pet", "eye"]},
+    {"filename" : "cat4.jpg", "keywords" : ["cat", "animal", "pet", "hand", "cute"]},
+    {"filename" : "dog1.jpg", "keywords" : ["dog", "animal", "pet", "smile"]},
+    {"filename" : "dog2.jpg", "keywords" : ["dog", "animal", "pet", "eye"]},
+    {"filename" : "fox.jpg", "keywords" : ["fox", "animal", "eye", "big ear"]},
+    {"filename" : "panda.jpg", "keywords" : ["panda", "animal", "eat", "hand"]},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    query = request.args.get("q", "").lower()
+    results = []
+    for item in images:
+        # found = False
+        # for keyword in item["keywords"]:
+        #     if query in keyword:
+        #         found = True
+        #         # break # 이미지 하나만 찾을 때 쓰기
+        # if found:
+        #     image_url = url_for('static', filename=f'img/{item["filename"]}')
+        #     results.append(image_url)
+        
+        # pythonic하게 한줄로
+        if any(query in keyword for keyword in item["keywords"]):
+            image_url = url_for('static', filename=f'img/{item["filename"]}')
+            results.append(image_url)
+
+    # return jsonify({"url": results}) # 순수 backend 개발자는 여기까지
+    return render_template("result.html", query=query, results=results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ 5.Project/2.pixabay/5.frontend_admin_restapi/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, url_for, request, send_from_directory, jsonify
+
+app = Flask(__name__)
+
+images = [
+    {"filename" : "cat.jpg", "keywords" : ["cat", "animal", "pet", "cute", "baby"]},
+    {"filename" : "cat2.jpg", "keywords" : ["cat", "animal", "pet", "two", "baby", "sleep"]},
+    {"filename" : "cat3.jpg", "keywords" : ["cat", "animal", "pet", "eye"]},
+    {"filename" : "cat4.jpg", "keywords" : ["cat", "animal", "pet", "hand", "cute"]},
+    {"filename" : "dog1.jpg", "keywords" : ["dog", "animal", "pet", "smile"]},
+    {"filename" : "dog2.jpg", "keywords" : ["dog", "animal", "pet", "eye"]},
+    {"filename" : "fox.jpg", "keywords" : ["fox", "animal", "eye", "big ear"]},
+    {"filename" : "panda.jpg", "keywords" : ["panda", "animal", "eat", "hand"]},
+]
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/api/search')
+def search():
+    query = request.args.get("q", "").lower()
+    results = []
+    
+    for item in images:
+        
+        if any(query in keyword for keyword in item["keywords"]):
+            image_url = url_for('static', filename=f'img/{item["filename"]}')
+            results.append(image_url)
+
+    return jsonify({"url": results}) # 순수 backend 개발자는 여기까지
+
+if __name__ == '__main__':
+    app.run(debug=True)
