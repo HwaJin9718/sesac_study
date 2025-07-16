a9e02a0ace299da5879d65e5f1c6345ee4f53668
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-17 01:56:12 +0900

    250716_Project_CRM - user paging 처리 기능 구현

--- 5.Project/5.CRMProject/backend/api/user_api.py
+++ 5.Project/5.CRMProject/backend/api/user_api.py
@@ -1,21 +1,60 @@
-from flask import Blueprint, jsonify, send_from_directory
+from flask import Blueprint, jsonify, send_from_directory, request, current_app
+# current_app : main.html 접근을 위해 app에 있는 static 폴더로 접근 필요
+# from app import app 시 순환 참조 오류 발생, 현재 활성화된 app을 가져오기 위해 current_app 사용
 import backend.database.user_db as db
 
 user_api = Blueprint('user', __name__, static_folder='../../static/pages/user/')
 
+#  처음 user 데이터 전달
 @user_api.route('/')
 def get_users_data():
-    users = db.get_users()
-    # print(users)
-    return jsonify({'users' : users})
 
+    # 페이징 처리
+    current_page = 1 # 기본 페이지
+    items_per_page = 20 # 한페이지의 item 개수
+
+    users = db.get_users_paging(current_page, items_per_page)
+    user_count = db.get_user_count()
+    total_page = user_count // items_per_page 
+
+    # users = db.get_users()
+    # print(users)
+    return jsonify({'users' : users, 'total_page' : total_page})
+
+
+# 페이징 처리 시 user 페이지 이동
+@user_api.route('/<int:page>')
+def get_users_paging(page):
+    # 다른 페이지로 이동시 페이징 처리된 main.html로 이동
+    return send_from_directory(current_app.static_folder, 'main.html')
+
+
+# 페이징 처리 시 user 데이터 전달
+@user_api.route('/page/<int:page>')
+def get_users_paging_data(page):
+
+    # 페이징 처리
+    current_page = page # 현재 페이지
+    items_per_page = 20 # 한페이지의 item 개수
+
+    users = db.get_users_paging(current_page, items_per_page)
+    user_count = db.get_user_count()
+    total_page = user_count // items_per_page 
+
+    return jsonify({'users' : users, 'total_page' : total_page})
+
+
+# user 상세 페이지 이동
 @user_api.route('/<id>') # id str로 지정 : string:
 def user_info_page(id):
     return send_from_directory(user_api.static_folder, 'user_info.html')
 
+
+# 상세페이지 user 데이터 전달
 @user_api.route('/info/<id>')
 def get_user_by_id_date(id):
-    print('받아온 id :', id)
+    # print('받아온 id :', id)
     user = db.get_user_by_id(id)
     # print(user)
     return jsonify({'user' : user})
+
--- 5.Project/5.CRMProject/backend/database/user_db.py
+++ 5.Project/5.CRMProject/backend/database/user_db.py
@@ -9,6 +9,7 @@ def get_connection():
     return conn
 
 
+# 전체 users 조회
 def get_users():
     conn = get_connection()
     cursor = conn.cursor()
@@ -22,6 +23,33 @@ def get_users():
     return users
 
 
+# 전체 users 조회 + paging 처리
+def get_users_paging(current_page, items_per_page):
+    offset = (current_page - 1) * items_per_page # 페이지가 이동될 때 몇개째부터 출력할지
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users LIMIT ? OFFSET ?", (items_per_page, offset))
+    users = cursor.fetchall()
+    conn.close()
+
+    # dict 형태로 변환
+    users = [dict(u) for u in users]
+
+    return users
+
+
+# 전체 users의 count 조회
+def get_user_count():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users")
+    user_count = cursor.fetchone()[0]
+    conn.close()
+
+    return user_count
+
+
+# id로 users 조회
 def get_user_by_id(id):
     conn = get_connection()
     cursor = conn.cursor()
