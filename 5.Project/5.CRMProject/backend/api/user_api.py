fbd111ae0d4b8b2758de0b5d1184440b62027b42
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-07-17 23:33:57 +0900

    250717_Project_CRM - user search & paging 처리 기능 구현

--- 5.Project/5.CRMProject/backend/api/user_api.py
+++ 5.Project/5.CRMProject/backend/api/user_api.py
@@ -15,7 +15,12 @@ def get_users_data():
 
     users = db.get_users_paging(current_page, items_per_page)
     user_count = db.get_user_count()
-    total_page = user_count // items_per_page 
+    total_page = 0 
+
+    if user_count % items_per_page == 0:
+        total_page = user_count // items_per_page
+    else:
+        total_page = (user_count // items_per_page) + 1
 
     # users = db.get_users()
     # print(users)
@@ -39,9 +44,82 @@ def get_users_paging_data(page):
 
     users = db.get_users_paging(current_page, items_per_page)
     user_count = db.get_user_count()
-    total_page = user_count // items_per_page 
+    total_page = 0 
+
+    if user_count % items_per_page == 0:
+        total_page = user_count // items_per_page
+    else:
+        total_page = (user_count // items_per_page) + 1
 
     return jsonify({'users' : users, 'total_page' : total_page})
+
+
+# 검색 및 페이징 처리 시 user 페이지 이동
+@user_api.route('/search/page/<int:page>')
+def get_users_search_paging(page):
+    # 다른 페이지로 이동시 페이징 처리된 main.html로 이동
+    return send_from_directory(current_app.static_folder, 'main.html')
+
+
+# 검색 및 페이징 처리 시 user 데이터 전달
+@user_api.route('/search')
+def users_search():
+
+    current_page = 1 # 처음 1 페이지 고정
+    items_per_page = 20 # 한페이지의 item 개수
+    users = []
+    user_count = 0
+    total_page = 0
+
+    search_name = request.args.get('name')
+    search_gender = request.args.get('gender')
+
+    if search_name and search_gender:
+        users = db.get_user_by_name_and_gender(search_name, search_gender, current_page, items_per_page)
+        user_count = db.get_user_by_name_and_gender_count(search_name, search_gender)
+    elif search_name:
+        users = db.get_user_by_name(search_name, current_page, items_per_page)
+        user_count = db.get_user_by_name_count(search_name)
+    elif search_gender:
+        users = db.get_user_by_gender(search_gender, current_page, items_per_page)
+        user_count = db.get_user_by_gender_count(search_gender)
+
+    if user_count % items_per_page == 0:
+        total_page = user_count // items_per_page
+    else:
+        total_page = (user_count // items_per_page) + 1
+
+    return jsonify({'users' : users, 'total_page' : total_page})
+
+
+@user_api.route('/search/<int:page>')
+def users_search_page(page):
+
+    current_page = page # 현재 페이지
+    items_per_page = 20 # 한페이지의 item 개수
+    users = []
+    user_count = 0
+    total_page = 0
+
+    search_name = request.args.get('name')
+    search_gender = request.args.get('gender')
+
+    if search_name is not None and search_gender is not None and search_name.strip() and search_gender.strip():
+        users = db.get_user_by_name_and_gender(search_name, search_gender, current_page, items_per_page)
+        user_count = db.get_user_by_name_and_gender_count(search_name, search_gender)
+    elif search_name is not None and search_name.strip():
+        users = db.get_user_by_name(search_name, current_page, items_per_page)
+        user_count = db.get_user_by_name_count(search_name)
+    elif search_gender is not None and search_gender.strip():
+        users = db.get_user_by_gender(search_gender, current_page, items_per_page)
+        user_count = db.get_user_by_gender_count(search_gender)
+
+    if user_count % items_per_page == 0:
+        total_page = user_count // items_per_page
+    else:
+        total_page = (user_count // items_per_page) + 1
+
+    return jsonify({'users' : users, 'total_page' : total_page, 'search_name': search_name, 'search_gender': search_gender})
 
 
 # user 상세 페이지 이동
--- 5.Project/5.CRMProject/backend/database/user_db.py
+++ 5.Project/5.CRMProject/backend/database/user_db.py
@@ -61,3 +61,77 @@ def get_user_by_id(id):
 
     return user
 
+
+# name으로 user 조회
+def get_user_by_name(name, current_page, items_per_page):
+    offset = (current_page - 1) * items_per_page # 페이지가 이동될 때 몇개째부터 출력할지
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE Name LIKE ? LIMIT ? OFFSET ?", ('%' + name + '%', items_per_page, offset))
+    users = cursor.fetchall()
+    conn.close()
+
+    users = [dict(u) for u in users]
+
+    return users
+
+# name으로 조회된 user의 count 
+def get_user_by_name_count(name):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users WHERE Name LIKE ?", ('%' + name + '%',))
+    user_count = cursor.fetchone()[0]
+    conn.close()
+
+    return user_count
+
+
+# gender로 user 조회
+def get_user_by_gender(gender, current_page, items_per_page):
+    offset = (current_page - 1) * items_per_page # 페이지가 이동될 때 몇개째부터 출력할지
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE Gender=? LIMIT ? OFFSET ?", (gender, items_per_page, offset))
+    users = cursor.fetchall()
+    conn.close()
+
+    users = [dict(u) for u in users]
+
+    return users
+
+
+# gender로 조회된 user의 count 
+def get_user_by_gender_count(gender):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users WHERE Gender=?", (gender,))
+    user_count = cursor.fetchone()[0]
+    conn.close()
+
+    return user_count
+
+
+# name, gender로 user 조회
+def get_user_by_name_and_gender(name, gender, current_page, items_per_page):
+    offset = (current_page - 1) * items_per_page # 페이지가 이동될 때 몇개째부터 출력할지
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE Name LIKE ? AND Gender=? LIMIT ? OFFSET ?", ('%' + name + '%', gender, items_per_page, offset))
+    users = cursor.fetchall()
+    conn.close()
+
+    users = [dict(u) for u in users]
+
+    return users
+
+
+# name, gender로 조회된 user의 count 
+def get_user_by_name_and_gender_count(name, gender):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM users WHERE Name LIKE ? AND Gender=?", ('%' + name + '%', gender))
+    user_count = cursor.fetchone()[0]
+    conn.close()
+
+    return user_count
+
