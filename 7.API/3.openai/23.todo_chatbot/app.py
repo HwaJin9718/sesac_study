06db605c4b3d19c7010652f83cf8676f3b282b16
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2025-08-06 23:54:04 +0900

    250806_API - ToDo CURD 기능 구현

--- 7.API/3.openai/23.todo_chatbot/app.py
+++ 7.API/3.openai/23.todo_chatbot/app.py
@@ -5,30 +5,7 @@ app = Flask(__name__, static_folder='public', static_url_path='')
 # 내 메모 리스트 답을 곳
 todos = []
 
-@app.route('/')
-def home():
-    return app.send_static_file('index.html')
-
-
-@app.route('/api/todo', methods=['GET'])
-def get_todos():
-    return jsonify({'error': '아직 구현 전'})
-
-
-@app.route('/api/todo', methods=['POST'])
-def add_todo():
-    return jsonify({'error': '아직 구현 전'})
-
-
-@app.route('/api/todo/<int:todo_id>', methods=['PUT'])
-def toggle_todo(todo_id):
-    return jsonify({'error': '아직 구현 전'})
-
-
-@app.route('/api/todo/<int:todo_id>', methods=['DELETE'])
-def delete_todos(todo_id):
-    return jsonify({'error': '아직 구현 전'})
-
+next_id = 1 # ToDo 넘버링
 
 # 미션1. todo CRUD 완성
 # 미션2. 챗봇을 front에 추가하고 /api/chat 을 추가
@@ -36,6 +13,79 @@ def delete_todos(todo_id):
 # 미션2-1. 어떻게 라우트를 분리할까? todo 서비스와 chat 서비스를 분리하자
 #        routes를 분리하려면? blueprint 도입
 # 미션3. 채팅 아무 말이나 받아서 GPT에게 주고, 요청 받아서 
+
+@app.route('/')
+def home():
+    return app.send_static_file('index.html')
+
+
+@app.route('/api/todo', methods=['GET'])
+def get_todos():
+    return jsonify({'todos': todos})
+
+
+# todo id로 단일 조회
+@app.route('/api/todo/<int:todo_id>', methods=['GET'])
+def get_todo(todo_id):
+
+    todo = ''
+    for todo in todos:
+        if todo['id'] == todo_id:
+            todo = todo
+            break
+
+    return jsonify({'todo': todo})
+
+
+@app.route('/api/todo', methods=['POST'])
+def add_todo():
+
+    global next_id
+    todo = request.get_json()
+    print('입력된 todo', todo)
+
+    if todo:
+        todos.append({'id': next_id, "todo" : todo['textInput']})
+        next_id += 1
+        return jsonify({'success': 'ToDo 등록 완료'})
+    
+    elif todo is None or todo.length() == 0:
+        return jsonify({'error': 'ToDo 등록이 실패'})
+    
+    
+
+
+@app.route('/api/todo/<int:todo_id>', methods=['PUT'])
+def toggle_todo(todo_id):
+    input_todo = request.get_json()
+    print('입력된 todo :', input_todo['todo_text'])
+
+    if input_todo is None:
+        return jsonify({'error': 'ToDo 수정 요청값이 없습니다.'})
+    
+    for todo in todos:
+        if todo['id'] == todo_id:
+            todo['todo'] = input_todo['todo_text'].strip()
+            return jsonify({'success': 'ToDo 수정 완료'})
+
+
+
+@app.route('/api/todo/<int:todo_id>', methods=['DELETE'])
+def delete_todos(todo_id):
+    delete = False
+
+    for todo in todos:
+        if todo['id'] == todo_id:
+            # todo 삭제
+            todos.remove(todo)
+            delete = True
+            break
+    
+    if delete:
+        return jsonify({'success': 'ToDo 삭제 완료'})
+
+    return jsonify({'error': 'ToDo 삭제 실패'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- 7.API/3.openai/23.todo_chatbot/public/js/todo.js
+++ 7.API/3.openai/23.todo_chatbot/public/js/todo.js
@@ -4,5 +4,130 @@
 // PUT /api/todo/${id}
 // DELETE /api/todo/${id}
 
+document.addEventListener('DOMContentLoaded', () => {
+    getTodos();
+    closeModifyModal();
+})
 
+const todoForm = document.getElementById('todo-form')
+todoForm.addEventListener('submit', createTodo);
 
+const todoList = document.getElementById('todo-list')
+todoList.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (e.target.classList.contains('modify')) {
+        const todo_id = e.target.id
+        getTodo(todo_id)
+        // modifyTodo(todo_id)
+    } else {
+        const todo_id = e.target.id
+        deleteTodo(todo_id)
+    }
+})
+
+async function getTodos() {
+    const response = await fetch('/api/todo')
+    const todos = await response.json()
+    console.log('서버응답:', todos)
+
+    if (todos) {
+        displayTodos(todos.todos)
+    }
+}
+
+async function getTodo(todo_id) {
+    const response = await fetch(`/api/todo/${todo_id}`)
+    const todo = await response.json()
+    console.log('서버응답:', todo)
+
+    if (todo) {
+        openModifyModal(todo.todo)
+    }
+}
+
+function displayTodos(todos) {
+    // 조회 시 이전 todo list 제거 후 새로 등록
+    todoList.querySelectorAll('li').forEach(li => li.remove())
+
+    todos.forEach(todo => {
+        console.log(todo) // {id: 1, todo: '숙제'}
+        const new_li = document.createElement('li')
+        // new_li.textContent = todo.todo
+        new_li.innerHTML = `${todo.todo} <a class="modify" id="${todo.id}" href="">수정</a> | <a class="delete" id="${todo.id}" href="">삭제</a>`
+
+        todoList.appendChild(new_li)
+        
+    })
+}
+
+async function createTodo() {
+    const textInput = document.getElementById('text-input').value.trim();
+
+    if (!textInput) return;
+
+    const response = await fetch('/api/todo', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({textInput})
+    })
+    const result = await response.json()
+    console.log('POST result', result)
+
+    if (result.error) {
+        alert('ToDo 등록이 실패했습니다. 다시 시도해 주세요')
+    }
+
+    // 등록 성공 시 ToDo 목록 조회 호출
+    getTodos();
+}
+
+// 모달 창 띄우기
+function openModifyModal(todo) {
+    document.getElementById('todo-id').value = todo.id;
+    document.getElementById('todo-text').value = todo.todo;
+    document.getElementById('modify-modal').style.display = 'block';
+}
+
+// 모달창 닫기
+function closeModifyModal() {
+    document.getElementById('modify-modal').style.display = 'none';
+}
+
+const modal = document.getElementById('modify-body')
+modal.addEventListener('submit', () => {
+    const todo_id = document.getElementById('todo-id').value
+    const todo_text = document.getElementById('todo-text').value
+    // const todo = {'todo_id' : todo_id, 'todo_text': todo_text}
+    modifyTodo(todo_id, todo_text);
+})
+
+async function modifyTodo(todo_id, todo_text) {
+    // 수정
+    const response = await fetch(`/api/todo/${todo_id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({todo_text})
+    })
+    const result = await response.json()
+    if (result.error) {
+        alert('ToDo 수정이 실패했습니다. 다시 시도해 주세요')
+    }
+
+    // 수정 성공 시 ToDo 목록 조회 호출
+    getTodos();
+}
+
+async function deleteTodo(todo_id) {
+    // 삭제
+    const response = await fetch(`/api/todo/${todo_id}`, {
+        method: 'DELETE'
+    })
+    const result = await response.json()
+    if (result.error) {
+        alert('ToDo 삭제가 실패했습니다. 다시 시도해 주세요')
+    }
+
+    // 삭제 성공 시 ToDo 목록 조회 호출
+    getTodos();
+}
